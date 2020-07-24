c155fdc349922939af7623012aaf716a5c8b279b
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-07-24 13:18:56 -0300

    atualização sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ messae: 'Hello Word' }));
 
-module.exports = routes;
+export default routes;
